742d2bfe8344af6037cb294f02b500b3f1a1be36
Prateek Agrawal <prateek.agrawal@needl.ai>
2025-02-05 00:58:11 +0530

    replaced meta pixel implementatio with pixel library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,34 +1,35 @@
 import 'styles/globals.css';
-import { PlasmicRootProvider } from "@plasmicapp/react-web";
-import type { AppProps } from "next/app";
-import Head from "next/head";
-import Script from "next/script";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
+import { PlasmicRootProvider } from '@plasmicapp/react-web';
+import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+import ReactPixel from 'react-facebook-pixel';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
 
 const META_PIXEL_ID = process.env.NEXT_PUBLIC_META_PIXEL_ID;
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (META_PIXEL_ID) {
+      ReactPixel.init(META_PIXEL_ID);
+      ReactPixel.pageView(); // Track first page load
+
+      const handleRouteChange = () => {
+        ReactPixel.pageView(); // Track page views on route change
+      };
+
+      router.events.on('routeChangeComplete', handleRouteChange);
+      return () => {
+        router.events.off('routeChangeComplete', handleRouteChange);
+      };
+    }
+  }, [router.events]);
+
   return (
-    <PlasmicRootProvider Head={Head} disableLoadingBoundary={true}>
-      <Script
-        id='meta-pixel-base-code'
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '${META_PIXEL_ID}');
-            fbq('track', 'PageView');`,
-        }}
-      />
-      
+    <PlasmicRootProvider disableLoadingBoundary={true}>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
