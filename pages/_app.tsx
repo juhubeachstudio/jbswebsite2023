2da9ddeeaa1984344ead55701dad67d02b99236f
Prateek Agrawal <prateek.agrawal@needl.ai>
2025-02-05 01:05:33 +0530

    importing fb pixel only on client side dynamically

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,6 @@ import { PlasmicRootProvider } from '@plasmicapp/react-web';
 import type { AppProps } from 'next/app';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import ReactPixel from 'react-facebook-pixel';
 import 'slick-carousel/slick/slick-theme.css';
 import 'slick-carousel/slick/slick.css';
 
@@ -13,19 +12,27 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    if (META_PIXEL_ID) {
-      ReactPixel.init(META_PIXEL_ID);
-      ReactPixel.pageView(); // Track first page load
+    // Import react-facebook-pixel dynamically on the client side
+    import('react-facebook-pixel')
+      .then((ReactPixelModule) => {
+        const ReactPixel = ReactPixelModule.default;
+        if (META_PIXEL_ID) {
+          ReactPixel.init(META_PIXEL_ID);
+          ReactPixel.pageView();
 
-      const handleRouteChange = () => {
-        ReactPixel.pageView(); // Track page views on route change
-      };
+          const handleRouteChange = () => {
+            ReactPixel.pageView();
+          };
 
-      router.events.on('routeChangeComplete', handleRouteChange);
-      return () => {
-        router.events.off('routeChangeComplete', handleRouteChange);
-      };
-    }
+          router.events.on('routeChangeComplete', handleRouteChange);
+          return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+          };
+        } else {
+          console.error('Meta Pixel ID is not defined');
+        }
+      })
+      .catch((err) => console.error('Meta Pixel failed to load:', err));
   }, [router.events]);
 
   return (
